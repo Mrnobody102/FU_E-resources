f48e08d1c7e35b9820c333f9df1122bb0872b1ab
thuong <thuongdthe150682@fpt.edu.vn>
2023-10-06 00:00:43 +0700

    login feature

--- src/main/java/fpt/edu/eresourcessystem/controller/LoginController.java
+++ src/main/java/fpt/edu/eresourcessystem/controller/LoginController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import fpt.edu.eresourcessystem.service.AccountService;
 import fpt.edu.eresourcessystem.model.Account;
 import fpt.edu.eresourcessystem.exception.AccountNotExistedException;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @PropertySources(value = {@PropertySource("classpath:webconfig.properties")})
@@ -20,10 +22,33 @@ public class LoginController {
         this.accountService = accountService;
     }
 
-    @GetMapping("/login")
-    public String login(@ModelAttribute Account account) throws AccountNotExistedException {
-        if(account != null){
-            return "course/detail_course";
-        } else throw new AccountNotExistedException("Account not existed.");
+    @GetMapping({"/","/login"})
+    public String loginProcess(final Model model) throws AccountNotExistedException {
+        model.addAttribute("account", new Account());
+        return "auth/login";
+    }
+
+    @PostMapping({"/login"})
+    public String login(@ModelAttribute Account account){
+            if(null == account){
+                return "auth/login";
+            } else if("MANAGER".equals(account.getUsername())){
+
+                return "lib_manager/lib-manager_dashboard";
+            } else if ("LECTURER".equals(account.getUsername())) {
+                return "lecturer/lecturer_subjects";
+            }
+            return  "student/home";
+    }
+
+    @GetMapping({"/logout"})
+    public String logout(final Model model){
+        model.addAttribute("account", new Account());
+        return "auth/login";
+    }
+
+    @GetMapping({"/profile"})
+    public String profile(){
+        return "common/profile";
     }
 }
