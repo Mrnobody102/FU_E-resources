8a8bc8aa9fb24a8801d9e9feb311271c3fc6f5bc
thuong <thuongdthe150682@fpt.edu.vn>
2023-10-09 22:12:45 +0700

    delete account

--- src/main/resources/static/js/librarian.js
+++ src/main/resources/static/js/librarian.js
@@ -6,3 +6,33 @@ document.querySelector('form').addEventListener('submit', function (event) {
     // Thêm mã JavaScript xử lý khi nút được nhấn
     alert('Tài liệu đã được thêm thành công!');
 });
+
+
+$(document).ready(function() {
+
+    // $(".page-number").click(function() {
+    //
+    //     var firstName = $("#firstName").val();
+    //     var lastName = $("#lastName").val();
+    //
+    //     var pageIndex = $(this).html();
+    //
+    //     alert("/admin/contacts/" + pageIndex + "?firstName=" + firstName + "&lastName=" + lastName);
+    //
+    //     window.location = "/admin/contacts/" + pageIndex + "?firstName=" + firstName + "&lastName=" + lastName;
+    //
+    // });
+
+
+    $(".delete-account").click(function(){
+
+        var accountId = $(this).attr("id");
+
+        var result = confirm("Do you want delete this account?" + accountId);
+
+        if(result){
+            window.location = "/librarian/accounts/delete/"+accountId;
+        }
+    });
+
+});
