ef1b2dea5b80fce7e5964052a9ccb6aef5878d8e
MaiHoa36 <109661905+Chuyencuabim@users.noreply.github.com>
2023-10-14 01:17:39 +0700

    Valid Add account

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -108,3 +108,73 @@ function validate_updateCourse() {
     });
 }
 
+function validate_addAccount() {
+    $('#add-account').validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 2,
+                maxlength: 50
+            },
+            password: {
+                required: true,
+                minlength: 3,
+                maxlength: 30
+            },
+            email: {
+                required: true,
+                email: true,
+                maxlength: 100
+            },
+            name: {
+                required: true,
+                minlength: 2,
+                maxlength: 50
+            },
+            dateOfBirth: {
+                required: true,
+                date: true
+            },
+            campus: {
+                required: true
+            },
+            role: {
+                required: true
+            },
+        },
+        messages: {
+            username: {
+                required: "Username is required!",
+                minlength: "Username must have at least 2 characters!",
+                maxlength: "Username can have at most 50 characters!"
+            },
+            password: {
+                required: "Password is required!",
+                minlength: "Password must have at least 3 characters!",
+                maxlength: "Password can have at most 30 characters!"
+            },
+            email: {
+                required: "Email is required!",
+                email: "Please enter a valid email address!",
+                maxlength: "Email can have at most 100 characters!"
+            },
+            name: {
+                required: "Full Name is required!",
+                minlength: "Full Name must have at least 2 characters!",
+                maxlength: "Full Name can have at most 50 characters!"
+            },
+            dateOfBirth: {
+                required: "Date of Birth is required!",
+                date: "Please enter a valid date (YYYY-MM-DD format)!"
+            },
+            campus: {
+                required: "Campus is required!"
+            },
+            role: {
+                required: "Role is required!"
+            }
+        }
+    });
+}
+
+
