3e3cc12f218d7155529d2749e4c4f8f79db4e02d
Pham Quang Huy (k15 HL) <huypqhe151329@fpt.edu.vn>
2023-10-28 00:36:23 +0700

    Update All

---
+++ src/main/java/fpt/edu/eresourcessystem/controller/RestControllers/MailServiceRestController.java
@@ -0,0 +1,31 @@
+package fpt.edu.eresourcessystem.controller.RestControllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class MailServiceRestController {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    @RequestMapping("/sendEmail")
+    public String helloSpringBoot(){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("maihoa362001@gmail.com");
+        message.setTo("huypq1801@gmail.com");
+        message.setSubject("Subject : Sample Email Subject");
+        message.setText("Body : This is sample mail mail Body.");
+
+        javaMailSender.send(message);
+
+        System.out.println("Mail successfully sent..");
+
+        return "Your Mail sent successfully";
+    }
+
+}
