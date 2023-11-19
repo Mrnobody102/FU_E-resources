055167ce2348c4edca904aabbec190341896198e
Pham Quang Huy (k15 HL) <huypqhe151329@fpt.edu.vn>
2023-11-19 21:19:21 +0700

    Edit ckeditor

---
+++ src/main/java/fpt/edu/eresourcessystem/model/elasticsearch/EsCourse.java
@@ -0,0 +1,44 @@
+package fpt.edu.eresourcessystem.model.elasticsearch;
+
+import fpt.edu.eresourcessystem.model.Course;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+import java.io.Serializable;
+
+@Document(indexName = "documents")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class EsCourse implements Serializable {
+    @Id
+    private String courseId;
+    @Field
+    private String code;
+    @Field
+    private String name;
+    @Field
+    private String description;
+    @Field
+    private String lecturer;
+    @Field
+    private String lastModifiedDate;
+    @Field
+    private String createdBy;
+
+    public EsCourse(Course course) {
+        this.courseId = course.getId();
+        this.code = course.getCourseCode();
+        this.description = course.getDescription();
+        this.name = course.getCourseName();
+        this.lecturer = course.getLecturer().getAccount().getName();
+        this.lastModifiedDate = course.getLastModifiedDate();
+        this.createdBy = course.getCreatedBy();
+    }
+}
